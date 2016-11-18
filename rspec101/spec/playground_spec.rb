ec36301e7f8c72efdbbe75a4d90ca7a29171f1f6
janenyasoro <janenyasoro@gmail.com>
2016-11-18 19:06:48 +0300

    doing the tutorials on testing using cucumber,capybara and rspec

---
+++ rspec101/spec/playground_spec.rb
@@ -0,0 +1,15 @@
+ require_relative '../lib/playground'
+
+describe Playground do 
+	context 'when there are no chidren' do
+		let(:playground) {Playground.new(0)}
+
+	 it 'is quite boring place' do
+	  	mood = playground.mood
+	   expect(mood).to eq('boring')
+	end
+      it 'is empty' do
+      expect(playground).to be_empty
+     end
+  end	
+end
